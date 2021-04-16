947fe51822203300e9b0a5a918bad0f4306098a5
sergey <sergeysh2001@gmail.com>
2021-04-16 12:59:01 +0300

    continue

--- Containers/Vector.hpp
+++ Containers/Vector.hpp
@@ -7,6 +7,7 @@
 
 #include "VectorIterator.hpp"
 #include "Reverseiterator.hpp"
+#include <memory.h>
 
 namespace ft {
 
@@ -25,9 +26,129 @@ class Vector {
   typedef VectorIterator<const T> const_iterator;
   typedef ReverseIterator<VectorIterator<T> > reverse_iterator;
   typedef ReverseIterator<VectorIterator<const T> > const_reverse_iterator;
- private:
-  value_type
+ protected:
+  pointer array_;
+  allocator_type allocator_;
+  size_t size_;
+  size_t capacity_;
+ public:
+
+  // Constructor and destructors
+  explicit Vector(const Allocator& alloc = Allocator()): allocator_(alloc), size_(0), capacity_(0) {
+    array_ = allocator_.allocate(0);
+  }
+  explicit Vector(size_type count, const T& value = T(), const Allocator& alloc = Allocator()): allocator_(alloc), size_(count), capacity_(count) {
+    array_ = allocator_.allocate(size_);
+    for (size_type i = 0; i < size_; ++i) {
+      allocator_.construct(array_ + i, value);
+    }
+  }
+//  template< class InputIt>
+//      Vector(InputIt first, InputIt last, const Allocator& alloc = Allocator());
+  Vector(const Vector& other): allocator_(other.allocator_), size_(other.size_), capacity_(other.capacity_) {
+    array_ = allocator_.allocate(capacity_);
+    for (size_type i = 0; i < size_; ++i) {
+      allocator_.construct(array_ + i, *(other.array_ + i));
+    }
+  }
+  ~Vector() {
+    for (size_type i = 0; i < size_; ++i) {
+      allocator_.destroy(array_ + i);
+    }
+    allocator_.deallocate(array_, capacity_);
+  }
+
+  Vector &operator=(const Vector& other) {
+    if (this != &other) {
+     // clear();
+
+    }
+  }
+
+  // Assign
+  void assign(size_type count, const T& value);
+  template<class InputIt>
+  void assign(InputIt first, InputIt last);
+
+
+  // Element access
+  reference at(size_type pos);
+  const_reference at(size_type pos) const;
+  reference operator[](size_type pos);
+  const_reference operator[](size_type pos) const;
+  reference front();
+  const_reference front() const;
+  reference back();
+  const_reference back() const;
+
+  // Iterators
+  iterator begin() {
+    return iterator(array_);
+  }
+  const_iterator begin() const {
+    return const_iterator(array_);
+  }
+  iterator end() {
+    return iterator(array_ + size_);
+  }
+  const_iterator end() const {
+    return const_iterator(array_ + size_);
+  }
+  reverse_iterator rbegin() {
+    return reverse_iterator(end());
+  }
+  const_reverse_iterator rbegin() const {
+    return const_reverse_iterator(end());
+  }
+  reverse_iterator rend() {
+    return iterator(begin());
+  }
+  const_reverse_iterator rend() const {
+    return const_iterator(begin());
+  }
+
+  // Capacity functions
+  bool empty() const {
+    return begin() == end();
+  }
+  size_type size() const {
+    return size_;
+  }
+  size_type max_size() const {
+    return allocator_.max_size();
+  }
+  void reserve(size_type new_cap);
+  size_type capacity() const {
+    return capacity_;
+  }
+
+  // Modifiers
+  void clear();
+  iterator insert(iterator pos, const T& value);
+  void insert(iterator pos, size_type count,const T& value);
+  template<class InputIt>
+  void insert(iterator pos, InputIt first, InputIt last);
+  iterator erase(iterator pos);
+  iterator erase(iterator first, iterator last);
+  void push_back(const T& value);
+  void pop_back();
+  void resize(size_type count, T value = T());
+  void swap(Vector& other);
 };
+
+template<class T, class Alloc>
+bool operator==(const ft::Vector<T,Alloc>& lhs, const ft::Vector<T,Alloc>& rhs);
+template<class T, class Alloc>
+bool operator!=(const ft::Vector<T,Alloc>& lhs, const ft::Vector<T,Alloc>& rhs);
+template<class T, class Alloc>
+bool operator<(const ft::Vector<T,Alloc>& lhs, const ft::Vector<T,Alloc>& rhs);
+template<class T, class Alloc>
+bool operator>(const ft::Vector<T,Alloc>& lhs, const ft::Vector<T,Alloc>& rhs);
+template<class T, class Alloc>
+bool operator<=(const ft::Vector<T,Alloc>& lhs, const ft::Vector<T,Alloc>& rhs);
+template<class T, class Alloc>
+bool operator>=(const ft::Vector<T,Alloc>& lhs, const ft::Vector<T,Alloc>& rhs);
+
 
 }
 
--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #include <vector>
 
 int main() {
-  ft::Vector<int> a;
-  ft::Vector<int>::const_iterator aa;
-  std::cout << "Hello, World!" << std::endl;
+  ft::Vector<int>  a(2, 10);
+  for (ft::Vector<int>::reverse_iterator it = a.rbegin(); it != a.rend(); ++it) {
+    std::cout << *it << std::endl;
+  }
   return 0;
 }
