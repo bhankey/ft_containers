ec17a6b8670b8a4226109fbe416e4140482d8d99
sergey <sergeysh2001@gmail.com>
2021-04-29 15:33:03 +0300

    template for testing map and temporary main.cpp

---
+++ tests/MapTester.hpp
@@ -0,0 +1,15 @@
+//
+// Created by sergey on 29.04.2021.
+//
+
+#ifndef FT_CONTAINERS_TESTS_MAPTESTER_HPP_
+#define FT_CONTAINERS_TESTS_MAPTESTER_HPP_
+
+#include "print.hpp"
+
+template<typename Map>
+void TestMap() {
+  Map full;
+}
+
+#endif //FT_CONTAINERS_TESTS_MAPTESTER_HPP_
